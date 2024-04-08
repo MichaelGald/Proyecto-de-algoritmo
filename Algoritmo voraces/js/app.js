3cd11333e8dafc2605747e955f9178313d9fce8a
MichaelGald <michaelcom292@gmail.com>
2024-04-08 00:38:05 -0600

    first commit

---
+++ Algoritmo voraces/js/app.js
@@ -0,0 +1,3 @@
+const descricion = document.getElementById('descricion');
+const oracion = 'Este proyecto tratara de algoritmo voraces que ayudan al usurio resolver multiples problemas.';
+descricion.textContent = oracion;
