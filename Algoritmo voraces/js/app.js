44cf649b479f02c239643b183ecb45b6d6a2961c
MichaelGald <michaelcom292@gmail.com>
2024-04-08 23:06:38 -0600

    index comenzado

--- Algoritmo voraces/js/app.js
+++
@@ -1,3 +0,0 @@
-const descricion = document.getElementById('descricion');
-const oracion = 'Este proyecto tratara de algoritmo voraces que ayudan al usurio resolver multiples problemas.';
-descricion.textContent = oracion;
